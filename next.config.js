45a0f25de24bcb8fe35c6c4fc534b8a1cb00b0bd
Tian Yang <tianyang@taou.com>
2024-06-13 15:19:06 +0800

    Add allowOrigin

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["http://localhost:3000", "https://congenial-goldfish-9755gw6rj7hxrp5-3000.app.github.dev"],
+    }
+  }
+}
 
 module.exports = nextConfig;
